c9a475ca7c33410381ceb81ae6551a2542a034b0
lennynT02 <lenin.masapanta@epn.edu.ec>
2024-05-20 09:21:56 -0500

    Creacion constructor

--- Autos.java
+++ Autos.java
@@ -1,45 +1,56 @@
 public class Autos  {
     //atributos
-    String color;
-    String marca;
-    String modelo;
-    String velocidad;
-    String placa;
-    int  anio;
-    //---------------------------------------------------------------------------------
-    Autos  (String  nuevoColor){
-        color=nuevoColor;
+    private String color;
+    private String marca;
+    private String modelo;
+    private double velocidad;
+    private int anio;
+
+    Autos(String color, String marca, String modelo, double velocidad, int anio) {
+        this.color = color;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidad = velocidad;
+        this.anio = anio;
     }
-    String getColor(){
-        return color;
+
+    String getColor() {
+        return this.color;
     }
-    String  getMarca(){
-        return marca;
+
+    String getMarca() {
+        return this.marca;
     }
-    String getModelo(){
-        return modelo;
+
+    String getModelo() {
+        return this.modelo;
     }
-    String getVelocidad(){
-        return velocidad;
+
+    double getVelocidad() {
+        return this.velocidad;
     }
-    int getAnio(){return anio;}
-    //---------------------------------------------------------------------------------
-    void setColor (String newColor){
-        this.color=newColor;
+
+    int getAnio() {
+        return this.anio;
     }
-    void setMarca (String  newMarca){
-        this.marca=newMarca;
+
+    void setColor(String color) {
+        this.color = color;
     }
-    void setModelo (String newModelo){
-        this.modelo=newModelo;
+
+    void setMarca(String marca) {
+        this.marca = marca;
     }
-    void setVelocidad (String newVelocidad){
-        this.velocidad=newVelocidad;
+
+    void setModelo(String modelo) {
+        this.modelo = modelo;
     }
-    void setAnio (int newAnio){
-        this.anio=newAnio;
+
+    void setVelocidad(double velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    void setAnio(int anio) {
+        this.anio = anio;
     }
 }
-
-
-
